7cf739c9dea6bf26ff9c13939076d24c3a6d21cb
ar062800 <32943044+ar062800@users.noreply.github.com>
2017-10-27 14:59:32 -0700

    Add files via upload

--- BoscoBot.java
+++ BoscoBot.java
@@ -86,6 +86,30 @@ public class BoscoBot
 		{
 			response = "Farts are nice.";
 		}
+		else if(findKeyword(statement.toLowerCase(), "bell schedules") >= 0)
+		{
+			response = "These are our normal schedules:\n Daily Schedule: School begins at 8:00 a.m.\n First Bell 	7:50 a.m.\n FIRST BLOCK (A1/B1) 	8:00 - 9:30 a.m.\n Break 	9:30 - 9:40 a.m.\n SECOND BLOCK (A2/B2) 	9:40 - 11:05 a.m.\n Lunch 	11:05 - 12:05 p.m.\n THIRD BLOCK (A3/B3) 	12:05 - 1:30 p.m.\n Break 	1:30 - 1:40 p.m.\n FOURTH BLOCK (A4/B4) 	1:40 - 3:05 p.m.\n This is our normal schedule! To see others, type one of these keywords and press ENTER:\n 'Liturgy Schedule', 'Late Start Schedule', 'Morning Assembly Schedule', 'Afternoon Schedule', 'Standarized Test Prep Schedule'\n";
+		}
+		else if(findKeyword(statement.toLowerCase(), "schedule") >= 0)
+		{
+			response = "These are our normal schedules:\n Daily Schedule: School begins at 8:00 a.m.\n First Bell 	7:50 a.m.\n FIRST BLOCK (A1/B1) 	8:00 - 9:30 a.m.\n Break 	9:30 - 9:40 a.m.\n SECOND BLOCK (A2/B2) 	9:40 - 11:05 a.m.\n Lunch 	11:05 - 12:05 p.m.\n THIRD BLOCK (A3/B3) 	12:05 - 1:30 p.m.\n Break 	1:30 - 1:40 p.m.\n FOURTH BLOCK (A4/B4) 	1:40 - 3:05 p.m.\n This is our normal schedule! To see others, type one of these keywords and press ENTER:\n 'Liturgy Schedule', 'Late Start Schedule', 'Morning Assembly Schedule', 'Afternoon Schedule', 'Standarized Test Prep Schedule'\n";
+		}
+		else if(findKeyword(statement.toLowerCase(), "bell") >= 0)
+		{
+			response = "These are our normal schedules:\n Daily Schedule: School begins at 8:00 a.m.\n First Bell 	7:50 a.m.\n FIRST BLOCK (A1/B1) 	8:00 - 9:30 a.m.\n Break 	9:30 - 9:40 a.m.\n SECOND BLOCK (A2/B2) 	9:40 - 11:05 a.m.\n Lunch 	11:05 - 12:05 p.m.\n THIRD BLOCK (A3/B3) 	12:05 - 1:30 p.m.\n Break 	1:30 - 1:40 p.m.\n FOURTH BLOCK (A4/B4) 	1:40 - 3:05 p.m.\n This is our normal schedule! To see others, type one of these keywords and press ENTER:\n 'Liturgy Schedule', 'Late Start Schedule', 'Morning Assembly Schedule', 'Afternoon Schedule', 'Standarized Test Prep Schedule'\n";
+		}
+		else if(findKeyword(statement.toLowerCase(), "twitter") >= 0)
+		{
+			response = "Follow our Twitter account @WeAreBoscoTech";
+		}
+		else if(findKeyword(statement, "instagram") >= 0)
+		{
+			response = "Follow our Instagram account @wearebosotech";
+		}
+		else if(findKeyword(statement, "athletics") >= 0)
+		{
+			response = "Bosco Tech is a member of the California Interscholastic Federation (CIF) and the Catholic Athletic Association (CAA). The Tigers compete with other high schools in nine interscholastic sports: baseball, basketball, cross-country, football, golf, soccer, tennis, track & field, and volleyball.";
+		}
 		else if(findKeyword(statement, "discover") >= 0
 			||findKeyword(statement, "discover the tech") >= 0)
 		{
@@ -113,7 +137,7 @@ public class BoscoBot
 	    System.out.printf("%c", result);
 	    try
 			{
-	        Thread.sleep(50);//0.5s pause between characters
+	        Thread.sleep(25);//0.5s pause between characters
 	    }
 			catch(InterruptedException ex)
 			{
